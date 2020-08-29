dd8cb67723111cafdfe7b1f111d6cbfedbaac05e
cai <caixuwu@outlook.com>
2020-08-29 14:53:33 +0800

    init commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-struct
+//
+//  Created by cai on 2020/8/29.
+//  Copyright © 2020 cai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
